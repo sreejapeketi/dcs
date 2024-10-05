6eef726b02093eb8d383eafbc52d38c5b888d714
sreeja <sreejapeketi@gmail.com>
2024-10-05 16:02:02 +0530

    Add theme related changes

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../header/header.jsx'
 import Footer from '../footer/footer.jsx'
+import Theme from '../home/theme.jsx'
 
 function Home() {
   return (
     <div className='home'>
         <Header />
-        <Footer />    
+        <Footer />  
+        <Theme />  
     </div>
   )
 }
---
+++ frontend/src/components/home/theme.jsx
@@ -0,0 +1,15 @@
+document.addEventListener('LoadContent', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+
+    // Load saved theme from local storage
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    document.documentElement.setAttribute('data-theme', savedTheme);
+
+    themeToggle.addEventListener('click', () => {
+        const currentTheme = document.documentElement.getAttribute('data-theme');
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+        document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+    });
+});
