52639349a3b2f6509109d3a573cdcd3e7c7ce54a
sreeja <sreejapeketi@gmail.com>
2024-10-19 12:54:58 +0530

    Add theme changes related to button

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../header/header.jsx'
 import Footer from '../footer/footer.jsx'
+import Theme from '../home/theme.jsx'
 
 function Home() {
   return (
     <div className='home'>
         <Header />
-        <Footer />    
+        <Footer />
+        <button id="theme-toggle">Toggle Dark/Light Mode</button>
+        <Theme />  
     </div>
   )
 }
